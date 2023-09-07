c88a4802485cec9266126bb1f6aafbb88bedae06
Смирнов Вячеслав <slava_smirnov@mail.ru>
2023-09-07 22:24:33 +0300

    feat: task to function

--- src/main/kotlin/module_one.kt
+++ src/main/kotlin/module_one.kt
@@ -1,15 +1,26 @@
 fun main(args: Array<String>) {
+    task_one()
+    task_two()
+    task_three()
+}
+
+fun task_one() {
 //  Задание №1
     var one: Any? = null
     one = 6
     var three = one / 2.5
     //в переменной "Three" тип Double
 
+}
+
+fun task_two() {
 //    Задание №2
     var one = "hello"
     var two = "aplana"
     println("$one $two")
+}
 
+fun tasl_three() {
 //    Задание №3
     var five: Char = '5'
     println(five.toString())
@@ -20,4 +31,3 @@ fun main(args: Array<String>) {
     println(five.code.toFloat())
     println(five.code.toLong())
 }
-
