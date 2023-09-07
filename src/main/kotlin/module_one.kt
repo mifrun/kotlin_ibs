9167ddeddab5709e069995868f39e3156bbb4004
Смирнов Вячеслав <slava_smirnov@mail.ru>
2023-09-07 22:05:05 +0300

    feat: task one

--- src/main/kotlin/module_one.kt
+++ src/main/kotlin/module_one.kt
@@ -1,7 +1,13 @@
 fun main(args: Array<String>) {
+    task_one()
+}
+
+fun task_one() {
     var one: Any? = null
     one = 6
     var three = one / 2.5
 //    в переменной "Three" тип Double
     println(three)
 }
+
+fun task_two
